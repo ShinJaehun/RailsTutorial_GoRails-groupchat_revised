749f3a4860e4243aaa4a6b83616647d8f72fb8b9
ShinJaehun <gunsmoke.shin@gmail.com>
2024-06-15 17:14:26 +0900

    기본 환경 bootstrap/devise 설정

---
+++ app/javascript/application.js
@@ -0,0 +1,11 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+import "@popperjs/core"
+import "bootstrap"
+
+console.log("넌 뒤졌어~")
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+console.log(popoverList)
+
